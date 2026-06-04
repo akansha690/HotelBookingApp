e310bfd6d85a6e78fc28d81443caa8840946f77c
akansha <akansha69@.com>
2026-06-04 14:51:28 +0530

    changes in config

--- AirbnbBookingService/src/models/sequelize.ts
+++ AirbnbBookingService/src/models/sequelize.ts
@@ -3,6 +3,10 @@ import {Sequelize} from 'sequelize';
 
 dotenv.config();
 
+console.log('DB_USER in sequelize file:', process.env.DB_USER);
+console.log('DB_PASSWORD in sequelize file:', process.env.DB_PASSWORD ? 'set' : 'NOT SET');
+console.log('DB_HOST in sequelize file:', process.env.DB_HOST);
+
 const sequelize = new Sequelize({
 
     username: process.env.DB_USER,
