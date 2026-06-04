72cafe746719f9aa9fef83c413732676a26485ff
akansha <akansha69@.com>
2026-06-04 15:31:30 +0530

    add changes in .env

--- AuthService/routers/router.go
+++ AuthService/routers/router.go
@@ -5,7 +5,7 @@ import (
 
 	middleware "AuthInGo/middlewares"
 	"AuthInGo/utils"
-
+    env "AuthInGo/config/env"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -19,8 +19,8 @@ func SetUpRouter(UserRouter Router) *chi.Mux {
 
 	chiRouter.Use(middleware.CORSMiddleware);
 	chiRouter.Use(middleware.RateLimiMiddleware)
-	chiRouter.With(middleware.JWTNextMiddleware).HandleFunc("/hotelservice/*", utils.ProxyToService("http://localhost:3000", "/hotelservice"))
-	chiRouter.With(middleware.JWTNextMiddleware).HandleFunc("/bookingservice/*", utils.ProxyToService("http://localhost:3001", "/bookingservice"))
+	chiRouter.With(middleware.JWTNextMiddleware).HandleFunc("/hotelservice/*", utils.ProxyToService(env.GetString("HOTEL_SERVICE_URL", "http://localhost:3000"), "/hotelservice"))
+	chiRouter.With(middleware.JWTNextMiddleware).HandleFunc("/bookingservice/*", utils.ProxyToService(env.GetString("BOOKING_SERVICE_URL", "http://localhost:3001"), "/bookingservice"))
 	
 	UserRouter.Register(chiRouter) 
 	return chiRouter
