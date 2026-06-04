98b9246ae433ff575865126ca81c57b183d5af61
akansha <akansha69@.com>
2026-06-04 15:43:32 +0530

    changes in main file

--- AuthService/main.go
+++ AuthService/main.go
@@ -4,12 +4,17 @@ import (
 	
 	"AuthInGo/app"
     config "AuthInGo/config/env"
-	
+	"fmt"
+	"os"
 )
 
 func main(){	
 	config.Load()
 	cfg:=app.ConfigConstructor()
 	app:= app.ApplicationConstructor(cfg)
+	if app == nil {
+        fmt.Println("Failed to initialize application")
+        os.Exit(1)
+    }
 	app.Run()
 }
