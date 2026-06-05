d074210cb8dcb7427915471a6dc3d29a7adf1c3b
akansha <akansha69@.com>
2026-06-05 10:15:38 +0530

    add cors as middlewares

--- AuthService/middlewares/cors.go
+++ AuthService/middlewares/cors.go
@@ -1,21 +1,26 @@
+
 package middleware
 
 import (
-	"net/http"
+   "net/http"
 )
 
 func CORSMiddleware(next http.Handler) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-    w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+    allowedOrigins := map[string]bool{
+      "http://localhost:5173": true,
+      "https://codewithvision.com": true,
+      "https://celebrated-genie-25cf9a.netlify.app": true,
+    }
 
-    w.Header().Set("Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS",
-    )
+    origin := r.Header.Get("Origin")
+    if allowedOrigins[origin] {
+      w.Header().Set("Access-Control-Allow-Origin", origin)
+    }
 
-    w.Header().Set("Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Idempotency-Key, X-User-ID",
-    )
+    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Idempotency-Key, X-User-ID")
 
     if r.Method == "OPTIONS" {
       w.WriteHeader(http.StatusOK)
